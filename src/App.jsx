77c2dcfb38479751995639a9c3d0e79225109ba0
AxelSalim <adjakidjememiaghe@gmail.com>
2024-10-22 16:22:40 +0100

    suppression du count

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import CakeView from './features/cake/CakeView'
 import IcecreamView from './features/icecream/IcecreamView'
 import UseView from './features/user/UseView'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <CakeView />
